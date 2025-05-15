d1b0a6cae72601c6d6df8a88e30f88e0e5229959
Michel Descoteaux <michel@locomotive.ca>
2025-05-15 16:48:33 -0400

    WIP - attempt to fix build process

--- build/grunt/config/terser.js
+++ build/grunt/config/terser.js
@@ -1,7 +1,4 @@
 module.exports = {
-    options: {
-        banner: '/*! <%= package.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-    },
     app: {
         files: {
             '<%= paths.js.dist %>/charcoal.advanced-search.min.js': [
