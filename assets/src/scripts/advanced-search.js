248f28cf1e4b4edf5d0afb846e6cd079de052729
Joel Alphonso <info@joelalphonso.com>
2026-02-20 12:14:24 -0500

    feat: add support for conditional filters in advanced search
    - Introduce support for `data-condition` attribute in filters
    - Update filters to include condition handling when defined

--- assets/src/scripts/advanced-search.js
+++ assets/src/scripts/advanced-search.js
@@ -410,6 +410,7 @@ class AdvancedSearch extends Charcoal.Admin.Widget {
         this.value    = value;
         this.operator = '=';
         this.type     = input.type;
+        this.condition = formField.data('condition') || null;
 
         this.parseInvalidBetween = function (name, value) {
             this.type     = 'date';
@@ -459,7 +460,8 @@ class AdvancedSearch extends Charcoal.Admin.Widget {
             value: this.value,
             operator: this.operator,
             type: this.type,
-            matching: this.matching
+            matching: this.matching,
+            condition: this.condition
         };
     }
 
@@ -654,12 +656,23 @@ class AdvancedSearch extends Charcoal.Admin.Widget {
                 }
             }
 
+            if (filter_obj.condition) {
+                const condition_value = filter_obj.condition.replace(/{{value}}/gi, filter_obj.value);
+                filters.push({
+                    conjunction: opts.conjunction || 'AND',
+                    table: filter_table,
+                    condition: condition_value,
+                })
+                return;
+            }
+
             filters.push({
                 conjunction: opts.conjunction || 'AND',
                 table: filter_table,
                 property: propName,
                 value: value_override.length ? value_override : filter_obj.value,
-                operator: filter_obj.operator
+                operator: filter_obj.operator,
+                condition: filter_obj.condition
             });
         });
 
